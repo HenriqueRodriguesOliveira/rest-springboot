e1bdd90ddee4aa4c3e079439c62f3e2b6e1fadde
Henrique Rodrigues Oliveira <henrique_krf@outlook.com>
2024-03-27 15:27:07 -0300

    first commit

---
+++ src/main/java/br/com/henriquedev/restspringbootwithjava/RestSpringbootWithJavaApplication.java
@@ -0,0 +1,13 @@
+package br.com.henriquedev.restspringbootwithjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestSpringbootWithJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestSpringbootWithJavaApplication.class, args);
+	}
+
+}
